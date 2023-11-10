3741e81cbbca0a3f0e4b899cb0bab61f8c603fb5
Elakiya030903 <elakiyathinakar@gmail.com>
2023-11-10 20:02:52 +0530

    commit

---
+++ NM news/README.md
@@ -0,0 +1,23 @@
+## World-News
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+A React JS app that provides a curation of news from over 50 top news sites around the world. 
+
+### Live
+https://nm-news.vercel.app/
+
+### Features
+- Users can access news from top sites(news outlets) around the world.
+- Users can access news from different countries and their official languages too.
+
+### App User Interface
+
+![Home Screen](https://i.imgur.com/EA92YOC.png)
+![news](src/assets/images/default_breaking_news.png)
+
+### Usage and Setup
+- Clone this repo. Go into the root folder and run *yarn* to install the dependencies.
+- Create a .env file in the root folder of this project.
+- Go to https://newsapi.org/, and get a free api key.
+- Set the api key you got as REACT_APP_API_KEY in the .env file.
+- Run *yarn start* to start the app.
---
+++ NM news/src/store/reducers/index.js
@@ -0,0 +1,75 @@
+import * as Actions from "../actions";
+
+const initialState = {
+  newsSource: "abc-news",
+  isLoading: true,
+  activeNewsSource: [],
+  tabData: [],
+  tabDataClone: [],
+  url: "sources",
+  api_key: process.env.REACT_APP_API_KEY,
+  proxy_url: process.env.REACT_APP_PROXY_URL,
+  isHamburgerIconVisible: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Actions.CHANGE_NEWS_SOURCE: {
+      return {
+        ...state,
+        newsSource: action.payload,
+      };
+    }
+    case Actions.CHANGE_IS_LOADING: {
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    }
+    case Actions.CHANGE_ACTIVE_NEWS_SOURCE: {
+      return {
+        ...state,
+        activeNewsSource: action.payload,
+      };
+    }
+    case Actions.POPULATE_TAB_DATA: {
+      return {
+        ...state,
+        tabData: action.payload,
+        tabDataClone: action.payload,
+      };
+    }
+    case Actions.SEARCH_TAB_DATA: {
+      const newTabData = state.tabDataClone.filter((tdc) =>
+        tdc.title.toLowerCase().startsWith(action.payload)
+      );
+      return {
+        ...state,
+        tabData: newTabData,
+      };
+    }
+    case Actions.RESET_TAB_DATA: {
+      return {
+        ...state,
+        tabData: state.tabDataClone,
+      };
+    }
+    case Actions.CHANGE_URL: {
+      return {
+        ...state,
+        url: action.payload,
+      };
+    }
+    case Actions.TOGGLE_HAMBURGER_ICON_VISIBILITY: {
+      return {
+        ...state,
+        isHamburgerIconVisible: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default reducer;
